03bc940bf45d500727bb47a04e8675feb1633173
bhavani <durgabhavani.mupparthi@gmail.com>
2024-08-28 07:12:05 +0000

    commit message

---
+++ src/main/java/com/example/movie/repository/MovieRepository.java
@@ -0,0 +1,17 @@
+package com.example.movie.repository;
+
+import com.example.movie.model.Movie;
+import java.util.ArrayList;
+
+public interface MovieRepository {
+
+    ArrayList<Movie> getMovies();
+
+    Movie getMovieById(int movieId);
+
+    Movie addMovie(Movie movie);
+
+    Movie updateMovie(int movieId, Movie movie);
+
+    void deleteMovie(int movieId);
+}
